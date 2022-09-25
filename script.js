45ad627a1f16ac486b9607b363b9f3943dc3979c
Jan Sørensen <32166535+jaso110@users.noreply.github.com>
2022-09-25 13:30:54 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 (function() {
+
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
   for (var name in names) {
     var firstLetter = names[name].charAt(0).toLowerCase();
     if (firstLetter === 'j') {
